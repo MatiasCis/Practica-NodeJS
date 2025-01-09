c6ce5c85916bc5fbf369fdfcde328ea48f53fb14
Matias <mgcm03@hotmail.com>
2025-01-09 19:39:30 -0300

    Update 6

---
+++ 05-multiplication/README.md
@@ -0,0 +1,7 @@
+# Node-ts-multiplication-app
+
+1. npm i
+
+2. npm run dev
+
+3. ej npx ts-node src/app.ts --base 8 --n tabladel8 --d output-tabla-8 --s
--- 05-multiplication/src/app.ts
+++ 05-multiplication/src/app.ts
@@ -1,5 +1,16 @@
+import { yarg } from "./config/plugins/args.plugin";
+import { ServerApp } from "./presentation/server-ap";
 
 
-const message: string = 'Hola mundo!';
 
-console.log(message)
+
+(async() => {
+    await main()
+})();
+
+async function main() {
+
+    const { b:base, l:limit, s:showTable, n:name, d:destination} = yarg;
+
+    ServerApp.run({ base, limit, showTable, name, destination});
+}
