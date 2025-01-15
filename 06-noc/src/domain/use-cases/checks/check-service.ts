a6270deda15f610705abc806f434b93bc8d21e5a
Matias <mgcm03@hotmail.com>
2025-01-15 01:03:59 -0300

    Update 10 send email attachments

--- 06-noc/src/domain/use-cases/checks/check-service.ts
+++ 06-noc/src/domain/use-cases/checks/check-service.ts
@@ -26,14 +26,22 @@ export class CheckService implements CheckServiceUseCase{
                 throw new Error(`Error on check service ${url}`);
             }
 
-            const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low)
+            const log = new LogEntity({
+                message: '',
+                level: LogSeverityLevel.low,
+                origin: 'check-service.ts'
+            })
             this.logRepository.saveLog(log);
             this.sucessCallback && this.sucessCallback();
             return true;
 
         } catch (error) {
             const errorMessage = `${url} is NOT ok. ${ error }`;
-            const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+            const log = new LogEntity({
+                message:errorMessage,
+                level:LogSeverityLevel.high,
+                origin: 'check-service.ts'
+            });
             this.logRepository.saveLog(log);
             this.errorCallback && this.errorCallback(errorMessage);
             return false;
--- 06-noc/src/presentation/server.ts
+++ 06-noc/src/presentation/server.ts
@@ -1,7 +1,9 @@
+import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasources";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service"
+import { EmailService } from "./email/email.service";
 
 
 const fileSystemRepository = new LogRepositoryImpl(
@@ -16,20 +18,29 @@ export class Server {
 
         console.log('Server started...')
 
-        CronService.createJob(
-            '*/5 * * * * *',
-            () => {
-                const url = 'https://google.com'
-                new CheckService(
-                    fileSystemRepository,
-                    () => console.log( `${ url } is OK!` ),
-                    ( error ) => console.log( error )
-                ).execute(url);
-                // new CheckService().execute('http://localhost:3000/');
+        const emailService = new EmailService();
+        emailService.sendEmailWithFileSystemLogs([
+            'matyxd03@gmail.com'
+        ])
+        
 
 
-            }
-        );
+
+
+        // CronService.createJob(
+        //     '*/5 * * * * *',
+        //     () => {
+        //         const url = 'https://google.com'
+        //         new CheckService(
+        //             fileSystemRepository,
+        //             () => console.log( `${ url } is OK!` ),
+        //             ( error ) => console.log( error )
+        //         ).execute(url);
+        //         // new CheckService().execute('http://localhost:3000/');
+        //         // console.log(envs)
+
+        //     }
+        // );
 
     }
 }
