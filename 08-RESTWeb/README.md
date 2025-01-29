37da08e42642e7d67c891ea37575026d9fa20c6b
Matias <mgcm03@hotmail.com>
2025-01-29 01:40:42 -0300

    Update 15

---
+++ 08-RESTWeb/README.md
@@ -0,0 +1,6 @@
+
+
+# DEV
+
+1. Clonar el .env.template y el crear el .env
+2. Ejecutar el comand ```docker compose up -d```
--- 08-RESTWeb/src/presentation/todos/routes.ts
+++ 08-RESTWeb/src/presentation/todos/routes.ts
@@ -1,26 +1,19 @@
 import { Router } from "express";
 import { TodosController } from "./controller";
 
-
-
 export class TodoRoutes {
 
-    static get routes(): Router{
+    static get routes(): Router {
 
         const router = Router();
         const todoController = new TodosController();
 
-        router.get('/', todoController.getTodos) 
-        router.get('/:id', todoController.getTodoById) 
-        router.post ('/', todoController.createTodo) 
-        router.put ('/:id', todoController.updateTodo) 
-        router.delete ('/:id', todoController.deleteTodo) 
-
-
-        
-
+        router.get('/', todoController.getTodos.bind(todoController));
+        router.get('/:id', todoController.getTodoById.bind(todoController));
+        router.post('/', todoController.createTodo.bind(todoController));
+        router.put('/:id', todoController.updateTodo.bind(todoController));
+        router.delete('/:id', todoController.deleteTodo.bind(todoController));
 
         return router;
     }
-
 }
