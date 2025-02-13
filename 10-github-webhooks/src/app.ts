0e0d0aeb6c215f3fcf7c4e4ac58bd3629023c18a
Matias <mgcm03@hotmail.com>
2025-02-12 22:31:02 -0300

    Update 23

--- 10-github-webhooks/src/app.ts
+++ 10-github-webhooks/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
+import { GithubSha256 } from './presentation/github/middlewares/github-sha256.middleware';
 
 
 (()=> {
@@ -15,6 +16,8 @@ function main() {
   const controller = new GithubController();
 
   app.use( express.json() );
+
+  app.use(GithubSha256.verifyGithubSignature)
 
   app.post('/api/github', controller.webhookHandler );
 
